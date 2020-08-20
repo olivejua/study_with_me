d79e540634ecb1ed75c2bcac8b8e33ddb852a24e
olivejoa <tmfrl4710@gmail.com>
2020-08-20 15:11:42 +0900

    에러복구 : 전부 리셋2

---
+++ src/main/java/com/ksk/project/study_with_me/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.ksk.project.study_with_me.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // JPA Auditing 활성화
+public class JpaConfig {}
---
+++ src/main/java/com/ksk/project/study_with_me/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.ksk.project.study_with_me.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
